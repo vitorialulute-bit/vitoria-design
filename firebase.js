dfc2206105e2a62881534159b7c1b9f53c826751
vitorialulute-bit <vitorialulute@gmail.com>
2026-08-27 10:28:34 -0300

    Configurar Firebase

--- firebase.js
+++ firebase.js
@@ -1,1 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyD6pPII_qQjbXJFma90ZNqkxcQmZhry7JY",
+  authDomain: "vitoria-design-436.firebaseapp.com",
+  projectId: "vitoria-design-436",
+  storageBucket: "vitoria-design-436.firebasestorage.app",
+  messagingSenderId: "832750119618",
+  appId: "1:832750119618:web:b8746cf2cd4b14916d295b"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export { app };
