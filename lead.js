a05d5db2b56bcfc02a2316c0e945d567b26ad0e0
vitorialulute-bit <vitorialulute@gmail.com>
2026-08-28 09:23:30 -0300

    lead.js
    Added Firebase Firestore integration to handle lead submissions.

--- lead.js
+++ lead.js
@@ -1,1 +1,21 @@
-alert("lead.js carregou");
+import { app } from "./firebase.js";
+
+import {
+  getFirestore,
+  collection,
+  addDoc
+} from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
+
+const db = getFirestore(app);
+
+const form = document.getElementById("leadForm");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  try {
+    await addDoc(collection(db, "leads"), {
+      nome: document.getElementById("nome").value,
+      telefone: document.getElementById("telefone").value,
+      email: document.getElementById("email").value,
+      servico: document.getElementById(
